f65ce9354687b8beeb1dd5a0a10e8a47575ce514
Mathias Dietz <mdietz@de.ibm.com>
2014-02-24 00:30:14 +0100

    easy update function added

--- GCodeInfo/src/de/dietzm/Position.java
+++ GCodeInfo/src/de/dietzm/Position.java
@@ -11,12 +11,24 @@ public class Position {
 	public Position(float x1,float y1){
 		x=x1;
 		y=y1;
-		//z=z1;
 	}
 	
 	public void updatePos(Position pos){
 		x=pos.x;
 		y=pos.y;
-		//z=pos.z;
+	}
+	
+	public void updatePos(float x1 , float y1){
+		x=x1;
+		y=y1;
+	}
+	
+	public float getDistance(float x1 , float y1){
+		float xmove = Math.abs(x - x1);
+		float ymove = Math.abs(y - y1);
+		if (xmove + ymove == 0)
+			return 0;
+		float move = (float) Math.sqrt((xmove * xmove) + (ymove * ymove));
+		return move;
 	}
 }
