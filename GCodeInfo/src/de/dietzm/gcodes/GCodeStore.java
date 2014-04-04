eadac97aa2aa95fbd25d1604e6724237a5078be8
Mathias Dietz <mdietz@de.ibm.com>
2014-04-04 22:17:28 +0200

    Use GCodeStore to store gcodes instead of Arraylist. This allows alternative implementations e.g. DatabaseStores

---
+++ GCodeInfo/src/de/dietzm/gcodes/GCodeStore.java
@@ -0,0 +1,41 @@
+package de.dietzm.gcodes;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class GCodeStore implements Iterable<GCode> {
+
+	private final ArrayList<GCode> list;
+	
+	protected GCodeStore(int size) {
+		 list = new ArrayList<GCode>(size);
+	}
+	public GCodeStore() {
+		 list = new ArrayList<GCode>();
+	}
+
+	public boolean add(GCode gc){
+		return list.add(gc);
+	}
+	
+	public int size(){
+		return list.size(); 
+	}
+	
+	public GCode get(int idx){
+		return list.get(idx);
+	}
+	
+	public void clear(){
+		list.clear();
+	}
+
+	@Override
+	public Iterator<GCode> iterator() {
+		return list.iterator();
+	}
+	
+	public void commit(){
+	
+	}
+}
