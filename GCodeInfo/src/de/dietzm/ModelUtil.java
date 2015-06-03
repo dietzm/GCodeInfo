270513d33566b8466e9b847a970660b8465e98ef
Mathias Dietz <gcode@dietzm.de>
2015-06-03 08:27:59 +0200

    ModelUtil to format Model output strings

---
+++ GCodeInfo/src/de/dietzm/ModelUtil.java
@@ -0,0 +1,40 @@
+package de.dietzm;
+
+public class ModelUtil {
+
+	public ModelUtil() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static String getFileSizeString(Model mod) {
+		long fs = mod.getFilesize();
+		String sz; 
+		if(fs >= 1000000){
+			float fs1 = fs / 1000000f;
+			sz=Constants.floatToString2(fs1)+"MB";
+		}else if(fs >=1000){
+			float fs1 = fs / 1000f;
+			sz=Constants.floatToString2(fs1)+"KB";	        			
+		}else{
+			sz=fs+" B";
+		}
+		return sz;
+	}
+	
+	public static String getPrintTimeString(Model mod){
+		return Constants.formatTimetoHHMMSS(mod.getTimeaccel(), new StringBuilder());
+	}
+	
+	public static String getPrintTimeString(Layer mod){
+		return Constants.formatTimetoHHMMSS(mod.getTimeAccel(), new StringBuilder());
+	}
+	
+	public static String getPrintTimeString(float tm){
+		return Constants.formatTimetoHHMMSS(tm, new StringBuilder());
+	}
+	
+	public static String getLayerHeightString(Model mod){
+		return Constants.floatToString2(mod.getAvgLayerHeight())+" mm";
+	}
+
+}
